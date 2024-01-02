d7c9692c84df2238866d5df969eaffa5c9209d61
Aayushbahadurpradhan <pradhanaayush9841@gmail.com>
2024-01-02 10:44:54 +0545

    second jan

--- lib/config/constants/api_endpoint.dart
+++ lib/config/constants/api_endpoint.dart
@@ -4,9 +4,9 @@ class ApiEndpoints {
   static const Duration connectionTimeout = Duration(seconds: 1000);
   static const Duration receiveTimeout = Duration(seconds: 1000);
   // For Windows
-  // static const String baseUrl = "http://10.0.2.2:3000/api/v1/";
+  static const String baseUrl = "http://10.0.2.2:3000/api/v1/";
   // For MAC
-  static const String baseUrl = "http://localhost:3000/api/v1/";
+  // static const String baseUrl = "http://localhost:3000/api/v1/";
   //static const String baseUrl = "http://192.168.4.4:3000/api/v1/";
 
   // ====================== Auth Routes ======================
